b94207eea4de471e909b736838fa3222d50ef99b
Italo Sergio <italosergio@mail.com>
2024-11-02 20:10:54 -0300

    setup: jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'js'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testMatch: ['**/tests/**/*.test.ts'],
+  testTimeout: 15000, // Define o timeout global para 15 segundos
+};
