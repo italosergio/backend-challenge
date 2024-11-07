8bba167cd2c66fd307f7ec490dca8eba5216af69
Italo Sergio <italosergio@mail.com>
2024-11-07 14:48:56 -0300

    feat: new end point /score - then catch posts by title score - by lib sentiment

--- src/controllers/postControllers.ts
+++ src/controllers/postControllers.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import post from '../models/Post';
+import Sentiment from 'sentiment';
 
 export const getPosts = async (req: Request, res: Response): Promise<Response> => {
   const { start, end } = req.query as { start?: string; end?: string };
@@ -42,3 +43,30 @@ export const getSortedPosts = async (req: Request, res: Response): Promise<Respo
     return res.status(500).json({ error: "Erro ao buscar posts." });
   }
 };
+
+export const phraseScore = async (req: Request, res: Response): Promise<Response> => {
+  const { start, end } = req.query as { start?: string; end?: string };
+
+  if (!start || !end) {
+    return res.status(400).json({ error: "Parâmetros 'start' e 'end' são obrigatórios." });
+  }
+
+  try {
+    const posts = await post.find({
+      created_utc: { $gte: new Date(start), $lte: new Date(end) },
+    }).sort({ created_utc: -1 });
+
+    const sentiment = new Sentiment();
+    const sortedListPostsWithPhrasesScore = posts
+      .map(post => ({
+        ...post.toObject(),
+        titleScore: sentiment.analyze(post.title).score
+      }))
+      .sort((a, b) => b.titleScore - a.titleScore)
+
+    return res.json({ sortedListPostsWithPhrasesScore});
+  } catch (error) {
+    console.error(error); // Log do erro
+    return res.status(500).json({ error: "Erro ao buscar posts." });
+  }
+}
--- src/routes/postRoutes.ts
+++ src/routes/postRoutes.ts
@@ -1,9 +1,10 @@
 import { Router, RequestHandler } from 'express';
-import { getPosts, getSortedPosts } from '../controllers/postControllers';
+import { getPosts, getSortedPosts, phraseScore } from '../controllers/postControllers';
 
 const router = Router();
 
 router.get('/', getPosts as unknown as RequestHandler);
 router.get('/sorted', getSortedPosts as unknown as RequestHandler);
+router.get('/score', phraseScore as unknown as RequestHandler);
 
 export default router;
